ad1760bee9fb9bc9a6dfd65800f183287967741a
maul-lq <mikosky01@gmail.com>
2023-04-07 15:35:29 +0700

    akses tabel riwayat ke datagridview

--- Proyek Riwayat Uang/App.cs
+++ Proyek Riwayat Uang/App.cs
@@ -13,7 +13,8 @@ namespace Proyek_Riwayat_Uang
 {
     public partial class App : Form
     {
-        AppDatabase db;
+        AppDatabase AppDB;
+
 
         public App()
         {
@@ -27,7 +28,30 @@ namespace Proyek_Riwayat_Uang
 
         private void App_Load(object sender, EventArgs e)
         {
+            try
+            {
+                AppDB = new AppDatabase();
+                AppDB.BukaKoneksi();
 
+                dataGridView_riwayat.DataSource = AppDB.read_riwayat();
+
+                // mengatur lebar kolom untuk kolom 'id'
+                dataGridView_riwayat.Columns["id"].Width = 50;
+
+                // mengatur lebar kolom untuk kolom 'uang'
+                dataGridView_riwayat.Columns["uang"].Width = Convert.ToInt32(100*1.2);
+
+                // mengatur lebar kolom untuk kolom 'tanggal'
+                dataGridView_riwayat.Columns["tanggal"].Width = Convert.ToInt32(80 * 1);
+
+                // mengatur lebar kolom untuk kolom 'catatan'
+                dataGridView_riwayat.Columns["catatan"].Width = Convert.ToInt32(150 * 2);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
         }
     }
 }
--- Proyek Riwayat Uang/AppDatabase.cs
+++ Proyek Riwayat Uang/AppDatabase.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows.Forms;
 using MySql.Data.MySqlClient;
 
 namespace Proyek_Riwayat_Uang
@@ -11,10 +12,39 @@ namespace Proyek_Riwayat_Uang
     public class AppDatabase
     {
         MySqlConnection _koneksi;
+        MySqlCommand _perintah;
+        MySqlDataAdapter _data;
+        StringBuilder strKoneksiRiwayat;
 
-        public AppDatabase(string strKoneksi) 
+        public AppDatabase() 
         {
-            _koneksi = new MySqlConnection(strKoneksi);
+            strKoneksiRiwayat = new StringBuilder();
+            strKoneksiRiwayat.Append("server=127.0.0.1;uid=root;pwd=;database=db_riwayat_uang");
+
+            _koneksi = new MySqlConnection(strKoneksiRiwayat.ToString());
+        }
+
+        public void BukaKoneksi()
+        {
+            _koneksi.Open();
+        }
+
+        public void TutupKoneksi()
+        {
+            _koneksi.Close();
+        }
+
+        public DataTable read_riwayat()
+        {
+            DataTable data = new DataTable();
+
+            string query = "SELECT * FROM `riwayat`";
+            _perintah = new MySqlCommand(query, _koneksi);
+            _data = new MySqlDataAdapter(query, _koneksi);
+
+            _data.Fill(data);
+
+            return data;
         }
     }
 }
